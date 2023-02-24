4546d9592e22bf5faa2ffd66b6c97b60294e803e
Wajid Khan <wajid.developer1@gmail.com>
2023-02-24 16:21:39 +0530

    First node project

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE demo;
+
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
+
+CREATE TABLE employees(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
